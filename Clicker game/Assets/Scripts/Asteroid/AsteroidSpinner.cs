77540b77f8b67cd718389f273a495155c119e269
awpas50 <awper9794@gmail.com>
2022-01-20 18:32:41 +1000

    New asteroid model

--- Clicker game/Assets/Scripts/Asteroid/AsteroidSpinner.cs
+++ Clicker game/Assets/Scripts/Asteroid/AsteroidSpinner.cs
@@ -8,7 +8,7 @@ public class AsteroidSpinner : MonoBehaviour
     [SerializeField] private GameObject asteroidVFX;
     void Start()
     {
-        float randomScaleVal = Random.Range(0.2f, 0.3f);
+        float randomScaleVal = Random.Range(0.22f, 0.28f);
         asteroidObject.transform.localScale = new Vector3(randomScaleVal, randomScaleVal, randomScaleVal);
         asteroidVFX.transform.localScale = new Vector3(randomScaleVal * 4f * 1.2f, randomScaleVal * 4f * 1.2f, randomScaleVal * 4f * 1.2f);
     }
