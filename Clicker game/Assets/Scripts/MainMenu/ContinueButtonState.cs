02a398e3e4c9bd7afa969a91a35b972714ea1b93
awpas50 <awper9794@gmail.com>
2022-02-05 13:30:30 +1000

    1.00.1

--- Clicker game/Assets/Scripts/MainMenu/ContinueButtonState.cs
+++ Clicker game/Assets/Scripts/MainMenu/ContinueButtonState.cs
@@ -12,10 +12,13 @@ public class ContinueButtonState : MonoBehaviour
     string path;
     private void Start()
     {
-#if !UNITY_WEBGL
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR
         var pathWithEnv = @"%userprofile%\AppData\Local\Motorland0123\Save\save1.txt";
         path = Environment.ExpandEnvironmentVariables(pathWithEnv);
-
+#elif UNITY_STANDALONE_OSX
+        path = Application.persistentDataPath + "/save1.txt";
+#elif UNITY_STANDALONE_LINUX
+        path = Application.persistentDataPath + "/Motorland/Save/save1.txt";
 #elif UNITY_WEBGL
         path = "/idbfs/Motorland0123" + "/save1.txt";
 #endif
--- Clicker game/Assets/Scripts/SaveSystem/SaveSystem.cs
+++ Clicker game/Assets/Scripts/SaveSystem/SaveSystem.cs
@@ -16,13 +16,13 @@ public static class SaveSystem
         BinaryFormatter formatter = new BinaryFormatter();
         // declare a path 
         string path;
-#if !UNITY_WEBGL
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR
         var pathWithEnv = @"%userprofile%\AppData\Local\Motorland0123\Save\save1.txt";
         path = Environment.ExpandEnvironmentVariables(pathWithEnv);
-        if (!Directory.Exists(path))
-        {
-            Directory.CreateDirectory(path);
-        }
+#elif UNITY_STANDALONE_OSX
+        path = Application.persistentDataPath + "/save1.txt";
+#elif UNITY_STANDALONE_LINUX
+        path = Application.persistentDataPath + "/Motorland/Save/save1.txt";
 #elif UNITY_WEBGL
         path = "/idbfs/Motorland0123" + "/save1.txt";
         if (!Directory.Exists(path))
@@ -42,9 +42,13 @@ public static class SaveSystem
     public static AllSaveData Load()
     {
         string path;
-#if !UNITY_WEBGL
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR
         var pathWithEnv = @"%userprofile%\AppData\Local\Motorland0123\Save\save1.txt";
         path = Environment.ExpandEnvironmentVariables(pathWithEnv);
+#elif UNITY_STANDALONE_OSX
+        path = Application.persistentDataPath + "/save1.txt";
+#elif UNITY_STANDALONE_LINUX
+        path = Application.persistentDataPath + "/Motorland/Save/save1.txt";
 #elif UNITY_WEBGL
         path = "/idbfs/Motorland0123" + "/save1.txt";
 #endif
